04eedcbcad3f301d319f9999ae42eb4e3b006d40
HeloisaJ <heloisananda21@gmail.com>
2025-10-27 18:57:36 -0300

    Adjusted the pipeline and graph_generation script for using the AssociationStrategy class

--- classes/weight_association.py
+++ classes/weight_association.py
@@ -1,5 +1,7 @@
 from classes.base_class import BaseClass
 from abc import abstractmethod
+import numpy as np
+import scipy.stats as st
 
 class AssociationStrategy(BaseClass):
 
@@ -15,10 +17,17 @@ class Pearson(AssociationStrategy):
     def eval(self, **kwargs):
         self.set_attributes(kwargs)
 
+        if np.std(self.x) == 0 or np.std(self.y) == 0:
+            return 0.0
+        
+        return np.corrcoef(self.x, self.y)[0, 1]
+
 class Spearman(AssociationStrategy):
 
     def eval(self, **kwargs):
         self.set_attributes(kwargs)
+
+
 
 class Kendall(AssociationStrategy):
 
