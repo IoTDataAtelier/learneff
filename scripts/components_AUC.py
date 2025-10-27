1ae5a9e1a22a4236831a4d06f40a517f31ef5baf
HeloisaJ <heloisananda21@gmail.com>
2025-10-27 08:55:47 -0300

    Initialized the code for calculating the AUC for the graph components associated with the graph edge weights

--- scripts/components_AUC.py
+++ scripts/components_AUC.py
@@ -2,10 +2,15 @@ from lib.functions import plot_AUC
 import numpy as np
 
 def graph_components_AUC(W_sorted: np.ndarray, time_windows: list, output_path:str):
-    t_before = 0
-    # AUC = 0
-    
-    for t in range(1, len(time_windows)):
+    AUC_total = 0
+    AUC_partial = 0
+
+    for t in range(0, len(time_windows)):
         Wt = W_sorted[t] # Array of tuples with (weight_value, quantity of components)
+        w0 = Wt[0]
+        w1 = Wt[len(Wt) - 1]
         
+        AUC_partial = (w1[0] - w0[0]) * w0[1]
+
         plot_AUC(time_windows[t], Wt, output_path)
+        AUC_total += AUC_partial
