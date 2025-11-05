6c184fe89ecbe1a93ff76e6b74b04e343651e1e8
Heloísa J. <101205056+HeloisaJ@users.noreply.github.com>
2025-11-05 09:13:58 -0300

    Feature/pipeline build (#11)
    * Implemented a initial version of the pipeline_builder with a function for data generation
    
    * Prepared all the functions of the pipeline builder
    
    * Pipeline Builder is working. Created a function run_all to execute the original pipeline
    
    * Removed the metaprogramming part from PipelineBuilder, because it was leading to unexpected behaviours
    
    * Added the variables for the original experiment inside the function run_all

---
+++ pipeline_builder.py
@@ -0,0 +1,103 @@
+from classes.base_class import BaseClass
+from lib.logger import success
+
+#---- Experiment Scripts ----
+from scripts.synthetic_data_generation import synthetic_data_generation
+from scripts.training_process import training_process
+from scripts.graph_generation import generate_graphs
+from scripts.graph_destruction import graph_edge_destruction
+from scripts.components_AUC import graph_components_AUC
+# -----------------------
+
+#---- Experiment Plots ----
+from lib.functions import plot_graph_destruction_heatmap
+# -----------------------
+
+#---- Auxiliar Classes ---
+from classes.data_generation import DataGenerationStrategy
+from classes.model import ModelStrategy
+from classes.algorithm import AlgorithmStrategy
+from classes.error_function import ErrorFunctionStrategy
+from classes.graph_gen import GraphGenerationStrategy
+from classes.weight_association import AssociationStrategy
+# -----------------------
+
+class PipelineBuilder(BaseClass):
+    
+    def __init__(self, state: dict):
+        self.pipeline = []
+        self.state = state
+
+    def data_generation(self, output_path: str, f_theta: DataGenerationStrategy, r_omega: DataGenerationStrategy, g_lambda: DataGenerationStrategy, N: int, D: int, noise: float, cov=None):
+        step = (
+            "Generating Synthetic Data",
+            lambda: (
+                self.state.update(dict(zip(
+                    ["filepath", "w_true"],
+                    synthetic_data_generation(output_path=output_path, f_theta=f_theta, r_omega=r_omega, g_lambda=g_lambda, N=N, D=D, noise=noise, cov=cov)))
+                )
+            ),
+        )
+        self.pipeline.append(step)
+
+    def model_training(self, output_path: str, D: int, T: int, lr: float, r_omega: DataGenerationStrategy, e_phi: ErrorFunctionStrategy, H: ModelStrategy, a: AlgorithmStrategy):
+        step = (
+            "Training Process",
+            lambda: self.state.update(
+                W=training_process(
+                    output_path=output_path, filepath=self.state["filepath"], D=D, T=T, lr=lr, r_omega=r_omega, e_phi=e_phi, H = H, a = a
+                )
+            ),
+        )
+        self.pipeline.append(step)
+
+    def graph_generation(self, output_path: str, q: GraphGenerationStrategy, corr: AssociationStrategy, S_w: int, M: int):
+        step = (
+            "Graph Generation and Graph Plot",
+            lambda: self.state.update(
+                graphs=generate_graphs(self.state["W"], output_path=output_path, q=q, corr=corr, S_w=S_w, M=M
+                )
+            ),
+        )
+        self.pipeline.append(step)
+
+    def graph_destruction(self, output_path:str):
+        step = (
+            "Graph Edge Destruction",
+            lambda: self.state.update(dict(zip(
+                ["n_components", "W_sorted"], 
+                graph_edge_destruction(G=self.state["graphs"], output_path=output_path))
+                )
+            ),
+        )
+        self.pipeline.append(step)
+
+    def plot_destruction_heatmap(self, output_path: str, T: int, S_w: int, M: int):
+        step = (
+            "Plot Graph Destruction Heatmap",
+            lambda: plot_graph_destruction_heatmap(
+                n_components=self.state["n_components"], output_path=output_path, T=T, S_w=S_w, M=M
+            )
+        )
+        self.pipeline.append(step)
+
+    def plot_destruction_AUC(self, output_path: str, time_windows: list):
+        step = (
+            "Plot Graph Components + AUC",
+            lambda: graph_components_AUC(
+                W_sorted=self.state["W_sorted"], time_windows=time_windows, output_path=output_path
+            )
+        )
+        self.pipeline.append(step)
+
+    def plot_CDF(self, **kwargs):
+        self.set_attributes(kwargs)
+        #self.pipeline.append(step)
+        pass
+
+    def execute_pipeline(self):
+        for index, (step_name, step_action) in enumerate(self.pipeline):
+            success(f"Executing step {index + 1}: {step_name}\n")
+            step_action()
+
+        success(f"\nPipeline execution completed successfully.")
