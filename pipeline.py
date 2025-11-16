12d2ea85481d55c654b878f635b94730044f19df
HeloisaJ <heloisananda21@gmail.com>
2025-11-15 21:44:42 -0300

    Created a function for generating a plot of the CDF of one graph

--- pipeline.py
+++ pipeline.py
@@ -38,7 +38,7 @@ def run_scene(pipeline: PipelineBuilder, scene: int, initial_path: str, D: int, 
     pipeline.normalize_data(norm_f = MinMaxNorm(), norm_state = "W")
 
     #corr_weights = {"pearson": Pearson(), "spearman": Spearman(), "kendall": Kendall(), "cross_correlation": CrossCorrelation(), "cossine": Cosine(), "icc": ICC()}
-    corr_weights = {"cross_correlation": CrossCorrelation(), "icc": ICC()}
+    corr_weights = {"cross_correlation": CrossCorrelation(), "pearson": Pearson()}
 
     for n, c in corr_weights.items():
         graphs_state = f"graphs_{n}"
@@ -61,11 +61,10 @@ def run_scene(pipeline: PipelineBuilder, scene: int, initial_path: str, D: int, 
             norm = None
 
         pipeline.graph_generation(q=Pairwise(), corr=c, S_w=S_W, M=M, graphs_state=graphs_state, output_path=graphs_output, norm_f=norm)
+        pipeline.plot_CDF(graphs_state=graphs_state, output_path=corr_output)
         pipeline.graph_destruction(graphs_state=graphs_state, n_components_state=n_components_state, W_sorted_state=W_sorted_state, output_path=plots_output)
         pipeline.plot_destruction_AUC(time_windows=list(range(0, T - S_W + 1, M)), W_sorted_state=W_sorted_state, output_path=AUC_output, norm_f=MinMaxNorm())
         pipeline.plot_destruction_heatmap(T=T, S_w=S_W, M=M, n_components_state=n_components_state, output_path=plots_output)
-        
-        # CDF
 
 
 def run_all(pipeline: PipelineBuilder):
@@ -100,19 +99,19 @@ def run_pipeline():
     pipeline = PipelineBuilder(state)
     
     #run_all(pipeline)
-    initial_path = "output/test"
+    initial_path = "output/test_3"
 
     run_scene(pipeline, 1, initial_path, D=11)
     pipeline.execute_pipeline()
-    pipeline.pipeline = []
+    #pipeline.pipeline = []
 
-    run_scene(pipeline, 2, initial_path, D=11, drop_w=0.5)
-    pipeline.execute_pipeline()
-    pipeline.pipeline = []
+    #run_scene(pipeline, 2, initial_path, D=11, drop_w=0.5)
+    #pipeline.execute_pipeline()
+    #pipeline.pipeline = []
 
-    run_scene(pipeline, 3, initial_path, D=21, drop_data=0.5)
-    pipeline.plot_train_val(partial_filepath=initial_path, scenes=[1, 2, 3], T=100)
-    pipeline.execute_pipeline()
+    #run_scene(pipeline, 3, initial_path, D=21, drop_data=0.5)
+    #pipeline.plot_train_val(partial_filepath=initial_path, scenes=[1, 2, 3], T=100)
+    #pipeline.execute_pipeline()
 
 if __name__ == "__main__":
     run_pipeline()
